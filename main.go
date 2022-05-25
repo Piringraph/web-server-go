91120de2cb936705b135bff5c3a3ec97144ba04b
Pirin <chuckdanorris123@gmail.com>
2022-05-25 00:45:53 -0400

    fixed typos and added newlines to form.html

--- main.go
+++ main.go
@@ -6,35 +6,37 @@ import (
 	"net/http"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request){
+func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "Name = %s \n", name)
+	fmt.Fprintf(w, "Address = %s \n", address)
 }
 
-func helloHandler(w http.ResponseWritter, r *http.Requqest) {
-if r.URL.Path != "/hello" {
-	http.Error(w, "404 not found", http.StatusNotFound)
-	return
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
-if r.Method != "GET" {
-	http.Error(w, "method is not supported", http.StatusNotFound)
-	return
-}
-fmt.Printf(w, "hello!")
+
 func main() {
-	fileserver := http.FileServer(http.Dir("./static"))
+	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Prinft("Starting server at port 8080\n")
+	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
