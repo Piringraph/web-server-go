f3cd79225254abfe1d5afb937695d8cdc69dfa03
Pirin <chuckdanorris123@gmail.com>
2022-05-25 00:07:01 -0400

    Added POST request successful message

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func formHandler(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func helloHandler(w http.ResponseWritter, r *http.Requqest) {
